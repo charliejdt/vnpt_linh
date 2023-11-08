2c68d303666aa182814f41d3ff8cb67e8eac64cb
duongtuancntt <duongtuan12a11@gmail.com>
2023-11-08 16:02:06 +0700

    check

--- utils/zalo.js
+++ utils/zalo.js
@@ -6,18 +6,19 @@ export default function LinkZalo() {
     if (typeof window !== "undefined") {
       const userAgent = window.navigator.userAgent;
 
-      if (userAgent.includes("Android")) {
-        setLink("https://zaloapp.com/qr/p/9cre2ojhk1b2");
-      } else if (
+      if (
         userAgent.includes("iPhone") ||
         userAgent.includes("iPad") ||
         userAgent.includes("iPod")
       ) {
         setLink("zalo://qr/p/9cre2ojhk1b2");
+      } else if (userAgent.includes("Android")) {
+        setLink("https://zaloapp.com/qr/p/9cre2ojhk1b2");
       } else {
         setLink("zalo://conversation?phone=0815117116");
       }
     }
   }, []);
+
   return link;
 }
