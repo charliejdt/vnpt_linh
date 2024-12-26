9b94b2d9f996226a48724ff3e84178fb2c7caae5
charliejdt <ronald0reallaliga@gmail.com>
2024-12-26 16:44:59 +0700

    Update

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,10 @@
+self.__NEXT_FONT_MANIFEST={
+  "pages": {},
+  "app": {
+    "/Users/tuanduong/Downloads/Code/vnpt_wifi/node_modules/next/dist/build/webpack/loaders/css-loader/src/index.js??ruleSet[1].rules[1].oneOf[3].use[1]!/Users/tuanduong/Downloads/Code/vnpt_wifi/node_modules/next/dist/build/webpack/loaders/next-font-loader/index.js??ruleSet[1].rules[1].oneOf[3].use[2]!/Users/tuanduong/Downloads/Code/vnpt_wifi/node_modules/next/font/google/target.css?{\"path\":\"pages/index.js\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}": [
+      "static/media/a34f9d1faa5f3315-s.p.woff2"
+    ]
+  },
+  "appUsingSizeAdjust": true,
+  "pagesUsingSizeAdjust": false
+}
